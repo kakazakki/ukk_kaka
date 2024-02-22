bab5ef5277a27b3236ffcd16a91e41cf13c98086
ReveeRosemary <exkyouko@gmail.com>
2024-02-22 09:34:01 +0700

    First Look

---
+++ resources/views/albums/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1> Albums </h1>
+<div class="row">
+    @foreach ($albums as $album)
+<div class="col-md-4">
+    <div class="card" style="width: 18rem;">
+        <img src="/storage/album_covers/{{$album->cover_image}}" height="200px" class="card-img-top" alt="Album Image">
+        <div class="card-body">
+            <h5 class="card-title">{{$album->name}}</h5>
+            <p class="card-text">{{$album->description}}</p>
+            <a href="{{route('albums.show' , $album->id)}}" class="btn btn-primary">View</a>
+            <form method="POST" action="{{ route('albums.destroy', $album->id) }}">
+                @csrf
+                @method('DELETE')
+
+                <button type="submit">Hapus</button>
+            </form>
+        </div>
+    </div>
+</div>
+    @endforeach
+</div>
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Http\Controllers\AlbumsController;
+use App\Http\Controllers\PhotosController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\LoginController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+// Path: routes\web.php
+
+Route::get('/albums', [AlbumsController::class, 'index']);
+Route::get('/albums/create', [AlbumsController::class, 'create'])->name('albums.create');
+Route::get('/albums/{album}', [AlbumsController::class, 'show'])->name('albums.show');
+Route::post('/albums', [AlbumsController::class, 'store'])->name('albums.store');
+Route::delete('/albums/{id}', [AlbumsController::class, 'destroy'])->name('albums.destroy');
+
+
+Route::get('/photo/upload/{album_id}', [PhotosController::class, 'create'])->name('photos.create');
+Route::post('/photo/upload', [PhotosController::class, 'store'])->name('photos.store');
+Route::get('/photo/{photo}', [PhotosController::class, 'show'])->name('photos.show');
+Route::delete('/photo/{id}', [PhotosController::class, 'destroy'])->name('photos.destroy');
+
+
+
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
